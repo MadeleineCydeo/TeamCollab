b053acd39c133c32a2527e628be0326309e7f6bd
AndrewBalint51 <andrew.balint@hotmail.com>
2023-05-17 11:09:09 -0700

    Added message for my baby

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -2,6 +2,9 @@ package utils;
 
 public class MainUtils {
 
-
+    public static void main(String[] args) {
+        System.out.println("I love my bubu baby!!!!!");
+        System.out.println("I want to live in Mexico with her!!");
+    }
 
 }
