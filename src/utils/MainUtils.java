64cb2ff94d2e8ce47bb3bee77df5d2ee9a4969c7
Bubbalicious23 <maddie_806@hotmail.com>
2023-05-17 13:06:46 -0700

    updated MainUtils class

--- src/utils/MainUtils.java
+++ src/utils/MainUtils.java
@@ -5,6 +5,16 @@ public class MainUtils {
     public static void main(String[] args) {
         System.out.println("I love my bubu baby!!!!!");
         System.out.println("I want to live in Mexico with her!!");
+
+
+        System.out.println("I love my dudu :)");
+        System.out.println("Can we pleaseee move to Mexico?");
+
+
+
+
     }
+
+
 
 }
